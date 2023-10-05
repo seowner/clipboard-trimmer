11f084cbff83424caa49c97a61e8967f7ddad1b8
seowner <41257893+seowner@users.noreply.github.com>
2023-10-05 03:14:09 -0400

    Add files via upload

---
+++ clipboard-trimmer/content.js
@@ -0,0 +1,10 @@
+document.addEventListener('paste', function (event) {
+    let paste = (event.clipboardData || window.clipboardData).getData('text');
+    const trimmed = paste.trim();
+    
+    event.preventDefault();  // Stop the data from being inserted into the page
+    
+    if (event.target.isContentEditable || event.target.tagName.toLowerCase() === 'textarea' || (event.target.tagName.toLowerCase() === 'input' && ['text', 'search', 'url', 'tel', 'password'].indexOf(event.target.type.toLowerCase()) > -1)) {
+        document.execCommand('insertText', false, trimmed);
+    }
+});
